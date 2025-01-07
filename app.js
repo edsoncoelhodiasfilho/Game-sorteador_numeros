d0727dfed7c0a902c347ac75c00c1778fbd89cb2
Edson C Dias Filho <edsoncdiasfilho@gmail.com>
2025-01-06 23:46:59 -0300

    Criando a função sortear

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear(){
+    let quantidade = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    console.log(`A quantidade é ${quantidade}`)
+    console.log(`A quantidade de é ${de}`)
+    console.log(`A quantidade até é ${ate}`)
+
+}
